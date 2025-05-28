49c615c61bc1374e8f19ff4538227f2b972e37a4
CabezadeBorrador <ivanbvonahn@gmail.com>
2025-05-27 19:35:30 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const letras = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ".split("");
+const letras = "ABCDEFGHIJLMNOPQRSTUV".split("");
 
 const categorias = [
   "Frutas", "Verduras", "Animales", "Países", "Ciudades", "Nombres de persona", "Profesiones", "Colores", "Partes del cuerpo", "Deportes",
-  "Comidas favoritas", "Películas famosas", "Programas de televisión", "Juguetes", "Videojuegos", "Dibujos animados", "Juegos de mesa", "Superhéroes", "Villanos", "Celebraciones",
+  "Comidas", "Películas", "Programas tv", "Juguetes", "Videojuegos", "Dibujos animados", "Juegos de mesa", "Superhéroes", "Villanos",
   "Cosas en la cocina", "Cosas en el baño", "Cosas en la escuela", "Cosas en la oficina", "Cosas que hay en una mochila", "Cosas que se encuentran en la playa", "Cosas que hay en una granja", "Cosas en un supermercado",
-  "Medios de transporte", "Marcas de autos", "Lugares turísticos", "Cosas en un aeropuerto", "Cosas en la carretera",
-  "Animales marinos", "Insectos", "Fenómenos naturales", "Planetas o cuerpos celestes", "Elementos de la tabla periódica", "Partes de una planta",
-  "Marcas de ropa", "Marcas de tecnología", "Marcas de comida rápida", "Celebridades", "Redes sociales", "Canciones populares", "Grupos musicales",
+  "Transporte", "Marcas", "Lugares turísticos", "Cosas en un aeropuerto", "Cosas en la carretera",
+  "Animales marinos", "Insectos", 
+  "Prendas de vestir", "Celebridades",
   "Cosas que vuelan", "Cosas que ruedan", "Cosas que hacen ruido", "Cosas que huelen mal", "Cosas que puedes romper", "Cosas suaves", "Cosas pegajosas"
 ];
 
@@ -34,15 +34,15 @@ function generarCategoria() {
 
 function iniciarTemporizador() {
   const display = document.getElementById("temporizador");
-  let tiempo = 30000; // 30 segundos en milisegundos
-  const intervalo = 10; // actualiza cada 10 ms
+  let tiempo = 30000; 
+  const intervalo = 10; 
 
   const countdown = setInterval(() => {
     tiempo -= intervalo;
     if (tiempo <= 0) {
       clearInterval(countdown);
       display.textContent = "00.00";
-      new Audio("chicharra.mp3").play(); // sonido final
+      new Audio("bugle_tune.ogg").play(); 
     } else {
       const segundos = Math.floor(tiempo / 1000).toString().padStart(2, '0');
       const milis = Math.floor((tiempo % 1000) / 10).toString().padStart(2, '0');
